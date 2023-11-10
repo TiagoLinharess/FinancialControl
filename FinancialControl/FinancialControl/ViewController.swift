8e0768bae08ebc4a0fb85cac6a545ceab9f11c4d
Tiago Linhares <tiagolinharessouza@gmail.com>
2023-11-10 10:07:56 -0300

    Creates Workspace

---
+++ FinancialControl/FinancialControl/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FinancialControl
+//
+//  Created by Tiago Linhares on 10/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
