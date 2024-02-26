0a3a88de2282fdcfb64888e67d81aff3713058e6
Tiago Linhares <tiagolinharessouza@gmail.com>
2024-02-26 20:04:16 -0300

    Implements testUtils

--- WeeklyBudgets/WeeklyBudgetsTests/Tests/Scenes/SingleWeekForm/SingleWeekFormViewTests.swift
+++ WeeklyBudgets/WeeklyBudgetsTests/Tests/Scenes/SingleWeekForm/SingleWeekFormViewTests.swift
@@ -5,6 +5,7 @@
 //  Created by Tiago Linhares on 19/11/23.
 //
 
+import Core
 @testable import WeeklyBudgets
 import SharpnezCore
 import SnapshotTesting
@@ -30,7 +31,7 @@ final class SingleWeekFormViewTests: XCTestCase {
     }
 
     func test_snapshot() throws {
-        let vc = get_swiftui_view_ready_for_snapshot(view: sut)
+        let vc = TestUtils.get_swiftui_view_ready_for_snapshot(view: sut)
         assertSnapshot(matching: vc, as: .image)
     }
     
