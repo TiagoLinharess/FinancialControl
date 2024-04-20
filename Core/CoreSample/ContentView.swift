97e2a893e033651a4aaed8ed3f214f9ef42a4a9c
Tiago Linhares <tiagolinharessouza@gmail.com>
2024-04-20 12:35:02 -0300

    Create Core

---
+++ Core/CoreSample/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  CoreSample
+//
+//  Created by Tiago Linhares on 20/04/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
---
+++ Core/CoreSample/CoreSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreSampleApp.swift
+//  CoreSample
+//
+//  Created by Tiago Linhares on 20/04/24.
+//
+
+import SwiftUI
+
+@main
+struct CoreSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
