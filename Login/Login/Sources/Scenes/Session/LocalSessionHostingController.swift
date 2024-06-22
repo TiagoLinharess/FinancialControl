ca1b4972681e34ad3303e141019ad254201f066b
Tiago Linhares <tiagolinharessouza@gmail.com>
2024-06-22 14:56:20 -0300

    Finish local authentication

---
+++ Login/Login/Sources/Scenes/Session/LocalSessionHostingController.swift
@@ -0,0 +1,10 @@
+//
+//  LocalSessionHostingController.swift
+//  Login
+//
+//  Created by Tiago Linhares on 22/06/24.
+//
+
+import SwiftUI
+
+final class LocalSessionHostingController: UIHostingController<LocalSessionView<LocalSessionViewModel>> { }
